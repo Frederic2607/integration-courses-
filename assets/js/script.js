2a0371b06ac667a4f9f78f2db2d304d64d54199a
Frédéric <desseaux_frederic@yahoo.fr>
2021-06-20 11:13:15 +0200

    not scroll with modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Afficher ou fermer la MODAL
+  // Afficher MODAL et empecher le scroll
   document.querySelector("#btn-signup").addEventListener("click", () => {
     document.querySelector(".modal").classList.remove("hidden");
+    document.querySelector("body").classList.add("no-scroll");
   });
 
+  // Cacher la MODAL et remettre le scroll
   document.querySelector(".modal-container i").addEventListener("click", () => {
     document.querySelector(".modal").classList.add("hidden");
+    document.querySelector("body").classList.remove("no-scroll");
   });
 
   // Envoie et récupération des données du formulaire
