561aefac1d595707f91ebc7ca9590ee2fac72470
Frédéric <desseaux_frederic@yahoo.fr>
2021-06-20 14:28:43 +0200

    add p:form trasmit and reset form with cross

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,34 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("body").classList.remove("no-scroll");
   });
 
+  // Détecter le scroll et ajouter la bordure au header
+  window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+    if (scroll !== 0) {
+      document.querySelector("header").classList.add("bottom-border");
+    } else {
+      document.querySelector("header").classList.remove("bottom-border");
+    }
+  });
+
+  // Reset le formulaire au click sur la croix
+  document.querySelector(".modal-cross").addEventListener("click", () => {
+    document.querySelector("#contact-form").reset();
+    document.querySelector("#submit-btn").removeAttribute("disabled");
+    document.querySelector(".form-send").classList.add("send-hidden");
+  });
+
   // Envoie et récupération des données du formulaire
   document
     .querySelector("#contact-form")
     .addEventListener("submit", async (event) => {
       event.preventDefault();
+
+      // désactiver le bouton de soumission
+      document
+        .querySelector("#submit-btn")
+        .setAttribute("disabled", "disabled");
+
       const data = {
         firstname: document.querySelector("#firstname").value,
         lastname: document.querySelector("#lastname").value,
@@ -23,8 +46,14 @@ document.addEventListener("DOMContentLoaded", () => {
         description: document.querySelector("#description").value,
       };
       const response = await axios.post(
-        "https://app-backend-tripadvisor.herokuapp.com/form",
+        // "https://app-backend-tripadvisor.herokuapp.com/form",
+        "http://localhost:3000/form",
         data
       );
+      if (response.status === 200) {
+        document.querySelector(".form-send").classList.remove("send-hidden");
+      } else {
+        alert("Erreur");
+      }
     });
 });
